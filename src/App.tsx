777624461f37d55908436f9aeb8099c397b9ebc2
Pablo Alves <pabloalves.dev@gmail.com>
2023-10-08 13:21:48 -0300

    j

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,67 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+interface IPlayer {
+  nome: string;
+  saldo: number;
+}
+
 function App() {
+  const initialMontant = 4500;
+  const [players, setPlayers] = useState<IPlayer[]>([] as IPlayer[]);
+  const [errorMessage, setErrorMessage] = useState({visible: true, message: ''});
+
+  const handleAction = (e: any) => {
+    const actionType = e.target.innerHTML as string;
+    const valueTo = Number(e.target.parentNode.previousElementSibling.children[0].value);
+    const name = e.target.parentNode.parentNode.id;
+    const newPlayers = players.map(a => {
+      if(a.nome === name && actionType === "Retirar") a.saldo = a.saldo-valueTo;
+      if(a.nome === name && actionType === "Inserir") a.saldo = a.saldo+valueTo;
+      return a
+    });
+    setPlayers(newPlayers);
+  }
+  const addNewPlayer = (e: any) => {
+    const nome = e.target.previousSibling.value;
+    if(!nome.length) setErrorMessage({visible: true, message:'Está em branco'});
+    else if(players.filter(a => a.nome === nome).length) alert('Já existe');
+    else setPlayers(prev => ([...prev, { nome, saldo: initialMontant }]));
+  }
+  useEffect(()=> {
+    setTimeout(() => setErrorMessage({visible: false, message: ''}), 3000)
+  }, [errorMessage])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <input className='add-input' type="text" />
+        <button type='submit' onClick={addNewPlayer} className='add-button'>Novo</button>
+        <p className="error-message">{errorMessage.message}</p>
+      </div>
+      <table>
+        <thead>
+          <tr>
+            <th>Nome</th>
+            <th>Saldo</th>
+            <th>Valor</th>
+            <th>Ações</th>
+          </tr>
+        </thead>
+        <tbody>
+          {players.map(p => (
+            <tr key={p.nome} id={p.nome}>
+              <td>{p.nome}</td>
+              <td>{p.saldo}</td>
+              <td><input type="text" /></td>
+              <td>
+                <button className='r' onClick={handleAction}>Inserir</button>
+                <button className='i' onClick={handleAction}>Retirar</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
